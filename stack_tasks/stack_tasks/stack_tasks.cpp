93d01ef4e4a627e088efe458095fae941ada3e88
MartinKonov <martinkonovv@gmail.com>
2021-11-08 15:49:42 +0200

    Stack Sorter- sort a stack using only stacks

--- stack_tasks/stack_tasks/stack_tasks.cpp
+++ stack_tasks/stack_tasks/stack_tasks.cpp
@@ -9,33 +9,10 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-string taskOne(string input) {
-
-	stack<char> something;
-	for (int i = 0;  i < input.size(); i++) {
-		something.push(input[i]);
-	}
-	size_t sthSize = something.size();
-
-	for (int i = 0; i < sthSize; i++) {
-		input[i] = (something.top());
-		something.pop();
-	}
-	return input;
-}
-
-
-
-
 void sortStack(stack<int>& firstStack) {
 
 	stack<int> result;
-
-	int helperOne = -1;
-	int helperTwo = -1;
-
 	int lowest = 999999;
-
 	int size = firstStack.size();
 
 	while (result.size() != size) {
@@ -45,58 +22,14 @@ void sortStack(stack<int>& firstStack) {
 
 		for (int i = 0; i < sz; i++) {
 			if (firstStack.top() < lowest) {
-				if (i != 0) {
-					if (helperOne < 0) {
-						helperOne = lowest;
-					}
-					else if (helperTwo < 0) {
-						helperTwo = lowest;
-					}
-					else {
-						if (helperOne >= helperTwo) {
-							CurrentResOne.push(helperTwo);
-							helperTwo = lowest;
-						}
-						else if (helperOne <= helperTwo) {
-							CurrentResOne.push(helperOne);
-							helperOne = lowest;
-						}
-
-					}
-				}
+				CurrentResOne.push(lowest);
 				lowest = firstStack.top();
 				firstStack.pop();
 			}
 			else {
-				if (helperOne < 0) {
-					helperOne = firstStack.top();
-					firstStack.pop();
-				}
-				else if (helperTwo < 0) {
-					helperTwo = firstStack.top();
-					firstStack.pop();
-				}
-				else {
-					if (helperOne >= helperTwo) {
-						CurrentResOne.push(helperTwo);
-						helperTwo = firstStack.top();
-						firstStack.pop();
-					}
-					else if (helperOne <= helperTwo) {
-						CurrentResOne.push(helperOne);
-						helperOne = firstStack.top();
-						firstStack.pop();
-					}
-				}
+				CurrentResOne.push(firstStack.top());
+				firstStack.pop();
 			}
-		}
-		if (helperOne > -1) {
-			CurrentResOne.push(helperOne);
-			helperOne = -1;
-		}
-		if (helperTwo > -1) {
-			CurrentResOne.push(helperTwo);
-			helperTwo = -1;
 		}
 		result.push(lowest);
 		lowest = 9999999;
@@ -111,16 +44,14 @@ void sortStack(stack<int>& firstStack) {
 }
 
 
-
-
 int main() {
 
 	stack<int> StackToSort;
 	StackToSort.push(1);
 	StackToSort.push(3);
-	StackToSort.push(6);
+	StackToSort.push(123);
 	StackToSort.push(2);
-	StackToSort.push(5);
-	StackToSort.push(4);
+	StackToSort.push(55);
+	StackToSort.push(-4);
 	sortStack(StackToSort);
 }
