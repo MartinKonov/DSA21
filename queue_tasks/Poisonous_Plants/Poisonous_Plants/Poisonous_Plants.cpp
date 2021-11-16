419274b120b2e65a36d0b7b13e4a7b3ae14dc5e3
MartinKonov <martinkonovv@gmail.com>
2021-11-16 20:29:18 +0200

    added Poisonous Plants

---
+++ queue_tasks/Poisonous_Plants/Poisonous_Plants/Poisonous_Plants.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+int poisonousPlants(vector<int> p) {
+
+	int days_passed = 0;
+
+	queue<int> plants, survivors;
+
+	int sz_vec = p.size();
+	for (int i = 0; i < sz_vec; i++) {
+		plants.push(p[i]);
+	}
+
+	while (true) {
+		
+		queue<int> cpy_of_plants = plants;
+		int last_front = plants.front();
+
+		survivors.push(plants.front());
+		plants.pop();
+
+		while (!plants.empty()) {
+
+			if (plants.front() > last_front) {
+				last_front = plants.front();
+				plants.pop();
+			}
+			else {
+				last_front = plants.front();
+				survivors.push(plants.front());
+				plants.pop();
+			}
+		}
+
+		plants = survivors;
+		
+		if (cpy_of_plants == plants) {
+			break;
+		}
+
+		while (!survivors.empty()) {
+			survivors.pop();
+		}
+
+		days_passed++;
+
+	}
+
+
+	return days_passed;
+}
+
+
+int main() {
+
+	vector<int> p = { 1, 1, 1 };
+	//6 7 8
+	cout << poisonousPlants(p);
+
+}
